a8c90454031249079da24d6df2b72db4912905e9
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-05 18:46:05 +0300

    Atualizações

--- app/components/ms_principal.php
+++ app/components/ms_principal.php
@@ -180,6 +180,11 @@
                 <div class="col-3 d-flex flex-column justify-content-center align-items-center" style="color:#fff; background-color:#30407f; height:100%; background-image:url(img/banner_bg_training.png); background-size:contain; background-repeat:no-repeat;">
                     <h6 style="font-size:30px;"> 45</h6>
                     <span style="font-size:10px;">Alunos</span>
+                    <div style="width:60%; height:40px; position:relative;">
+                        <div style="position:absolute; bottom:0; left:0; width:33%; height:10%; background-color:#e88016; float:left;"></div>
+                        <div style="position:absolute; bottom:0; left:33%; width:33%; height:45%; background-color:#76b42d; float:left; border:solid 1px #fff; border-bottom:0;"></div>
+                        <div style="position:absolute; bottom:0; left:66%; width:33%; height:80%; background-color:#2886c7; float:left;"></div>
+                    </div>
                 </div>
                 <div class="col-6 d-flex justify-content-center flex-column align-items-center">
                     <h6 style="text-align:center; color:#075595;"><?=$d->name?></h6>
