5ca626a111bb8fca36d6e3744f055700b814d5dc
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-05 19:23:38 +0300

    Atualizações

--- app/components/ms_principal.php
+++ app/components/ms_principal.php
@@ -259,7 +259,7 @@
                                 <i class="fa fa-calendar"></i> <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
                                 <i class="fa fa-users"></i> 122 Vagas
                             </small>
-                            <span class="bg-success p-1 text-dark bg-opacity-10">
+                            <span class="bg-success p-1 text-dark bg-opacity-25">
                                 <i class="fa fa-dolar"></i> LE 1000.00 Cost
                             </span>
                         </p>
