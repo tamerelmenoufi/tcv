576504dceeef48875a129b162a333449c0270073
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-08 14:36:20 +0300

    Atualizações

--- app/src/company/training.php
+++ app/src/company/training.php
@@ -73,9 +73,19 @@
                         <i class="fa fa-calendar"></i> <?=$Dic['Date']?>: <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
                         <i class="fa fa-users"></i> <?=$d->trainings?> <?=$Dic['Trained Opportunity']?>
                     </div>
-                    <div class="w-100" style="text-align:<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>; padding-<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>:5px; margin-top:5px;">
+                    <!-- <div class="w-100" style="text-align:<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>; padding-<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>:5px; margin-top:5px;">
                         <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
                             <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
+                        </span>
+                    </div> -->
+
+                    <div class="w-100 p-2" style="color:#054f8c; font-size:12px; text-align:<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>;">
+                        <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
+                            <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
+                        </span>
+                        <span class="text-bg-warning rounded p-1" style="margin-<?=(($_SESSION['lng'] == 'ar')?'right':'left')?>:5px;">
+                            <i class="fa-solid fa-face-flushed"></i>
+                            <?=$Dic[$d->status]?>
                         </span>
                     </div>
                 </small>
