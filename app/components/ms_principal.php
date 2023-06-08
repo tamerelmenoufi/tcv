79d06b7921243d10aed131c3e8a3a465863f0f19
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-08 14:13:35 +0300

    Atualizações

--- app/components/ms_principal.php
+++ app/components/ms_principal.php
@@ -252,8 +252,8 @@
                 </div>
                 <div class="row g-0">
                     <div class="col">
-                        <p class="d-flex justify-content-between flex-row card-text p-2" style="color:#054f8c; font-size:12px;">
-                            <small class="text-body-secondary">
+                        <p class="d-flex justify-content-between card-text p-2" style="color:#054f8c; font-size:12px;">
+                            <!-- <small class="text-body-secondary">
                                 <i class="fa fa-calendar"></i> <?=$Dic['Date']?>: <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
                                 <i class="fa fa-users"></i> <?=$d->trainings?> <?=$Dic['Trained Opportunity']?>
                             </small>
@@ -261,7 +261,20 @@
                                 <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
                                     <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
                                 </span>
-                            </span>
+                            </span> -->
+
+                            <small class="w-100">
+                                <div class="w-100">
+                                    <i class="fa fa-calendar"></i> <?=$Dic['Date']?>: <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
+                                    <i class="fa fa-users"></i> <?=$d->trainings?> <?=$Dic['Trained Opportunity']?>
+                                </div>
+                                <div class="w-100" style="text-align:<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>; padding-<?=(($_SESSION['lng'] == 'ar')?'left':'right')?>:5px; margin-top:5px;">
+                                    <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
+                                        <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
+                                    </span>
+                                </div>
+                            </small>
+
                         </p>
                     </div>
                 </div>
