7e641835c90237f4ec58848be1d68ea26457c189
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-08 14:24:14 +0300

    Atualizações

--- app/components/ms_principal.php
+++ app/components/ms_principal.php
@@ -253,11 +253,11 @@
                 <div class="row g-0">
                     <div class="col">
 
-                            <div class="text-body-secondary w-100" style="color:#054f8c; font-size:12px;">
+                            <div class="text-body-secondary w-100 p-2" style="color:#054f8c; font-size:12px;">
                                 <i class="fa fa-calendar"></i> <?=$Dic['Date']?>: <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
                                 <i class="fa fa-users"></i> <?=$d->trainings?> <?=$Dic['Trained Opportunity']?>
                             </div>
-                            <div class="w-100" style="color:#054f8c; font-size:12px; margin-top:5px; margin-bottom:5px; text-align:left;">
+                            <div class="w-100 p-2" style="color:#054f8c; font-size:12px; margin-top:5px; margin-bottom:5px; text-align:left;">
                                 <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
                                     <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
                                 </span>
