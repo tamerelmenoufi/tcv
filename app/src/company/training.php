71fd495130eb297b36643af0228b616517570c85
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-08 13:05:12 +0300

    Atualizações

--- app/src/company/training.php
+++ app/src/company/training.php
@@ -64,7 +64,7 @@
                 <small class="text-body-secondary">
                     <i class="fa fa-calendar"></i> <?=$Dic['Date']?>: <?=dataBr($d->initial_date)?> - <?=dataBr($d->final_date)?><br>
                     <i class="fa fa-users"></i> <?=$d->trainings?> <?=$Dic['Trained Opportunity']?><br>
-                    <span class="bg-success p-1 text-dark bg-opacity-25 rounded">
+                    <span class="bg-success p-1 text-dark bg-opacity-25 rounded" style="margin-top:5px;">
                         <i class="fa-solid fa-sack-dollar"></i> <?=$Dic['LE']?> <?=number_format($d->cost,2,'.',false)?> <?=$Dic['Cost']?>
                     </span>
                 </small>
