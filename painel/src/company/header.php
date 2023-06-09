76aa93cb2410fc24bb78925009a5febaf4b81284
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-09 16:16:15 +0300

    Atualizações

--- painel/src/company/header.php
+++ painel/src/company/header.php
@@ -7,12 +7,12 @@
         $d = mysqli_fetch_object($result);
 
 ?>
-<div class="card mb-3">
-    <div class="row g-0">
-        <div class="col-md-12">
-            <button class="btn btn-light retorno"><i class="fa-solid fa-angles-left"></i> <?=$Dic['Back']?></button>
-        </div>
+<div class="row g-0">
+    <div class="col-md-12">
+        <button class="btn btn-light retorno"><i class="fa-solid fa-angles-left"></i> <?=$Dic['Back']?></button>
     </div>
+</div>
+<div class="card mb-3">
     <div class="row g-0">
         <div class="col-md-4" style="position:relative">
             <div style="border:solid 0px red; position:absolute; left:0; right:0; top:0; bottom:0; background-size:contain; background-image:url(./src/volume/<?=$d->id?>/<?=$d->logo?>); background-repeat:no-repeat; background-position:center center;"></div>
