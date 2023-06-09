bcdec5e83776045baea364088424ada9b09041c0
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-10 00:33:42 +0300

    Atualizações

--- painel/src/dashboard/index.php
+++ painel/src/dashboard/index.php
@@ -1,39 +1,48 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
 
+
+    $query = "SELECT
+                    (select count(*) from company_training) as training,
+                    (select count(*) from company) as company,
+                    (select count(*) from trainee) as trainee,
+                    (select count(*) from treinee_admission) as admission
+            ";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+
+
 ?>
 
 
 <div class="row g-0">
     <div class="col-md-3">
         <div class="alert alert-success text-center m-2" role="alert">
-            <small>Empresas</small>
-            <h1>123</h1>
+            <small><?=$Dic['Company']?></small>
+            <h1><?=$d->company?></h1>
         </div>
     </div>
     <div class="col-md-3">
         <div class="alert alert-danger text-center m-2" role="alert">
-            <small>Empresas</small>
-            <h1>123</h1>
+            <small><?=$Dic['Training']?></small>
+            <h1><?=$d->training?></h1>
         </div>
     </div>
     <div class="col-md-3">
         <div class="alert alert-warning text-center m-2" role="alert">
-            <small>Empresas</small>
-            <h1>123</h1>
+            <small><?=$Dic['Trainne']?></small>
+            <h1><?=$d->trainne?></h1>
         </div>
     </div>
     <div class="col-md-3">
         <div class="alert alert-info text-center m-2" role="alert">
-            <small>Empresas</small>
-            <h1>123</h1>
+            <small><?=$Dic['Admission']?></small>
+            <h1><?=$d->admission?></h1>
         </div>
     </div>
 </div>
 
-<p>
-    Aqui entra o texto e as apresentações do sistema
-</p>
+
 <script>
     $(function(){
 
