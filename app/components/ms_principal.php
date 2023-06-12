aa0ae97e737623c6e118505caecdf3cced081d64
tamerelmenoufi <tamer@mohatron.com.br>
2023-06-13 02:26:10 +0300

    Instalação

--- app/components/ms_principal.php
+++ app/components/ms_principal.php
@@ -291,6 +291,14 @@
                                 </span>
                                 <?php
                                 }
+                                if($d->status == 'registred'){
+                                ?>
+                                <span class="text-bg-danger rounded p-1" style="margin-<?=(($_SESSION['lng'] == 'ar')?'right':'left')?>:5px;">
+                                    <i class="fa-solid fa-trash"></i>
+                                    <?=$Dic['Cancel']?>
+                                </span>
+                                <?php
+                                }
                                 ?>
                             </div>
 
